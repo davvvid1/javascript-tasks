81c854b46c0c294ab640fec96e47b8b5040f16a3
Dawid Zegar <davvvid1@gmail.com>
2016-12-19 15:38:30 +0100

    fix test file name

--- verify.sh
+++ verify.sh
@@ -1,3 +1,3 @@
 bundle exec rake db:create
 bundle exec rake db:test:prepare
-bundle exec rspec --format RspecJunitFormatter --out target/rspec.xml
+bundle exec rspec --format RspecJunitFormatter --out target/test-results.xml
